864ace885ae0ea052fe1baf34ade2a324033f8fc
Connor Wegrzyn <20263958@eastridingcollege.ac.uk>
2016-10-06 23:58:13 +0100

    untracked added

---
+++ WordFlick/Slot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using FarseerPhysics;
+
+namespace WordFlick
+{
+    class Slot
+    {
+        public bool full;
+        public Animation slotAnimation;
+        public String letter;
+           public Slot( Texture2D sprite, Vector2 position)
+            {
+            full = false;
+            slotAnimation = new Animation(sprite, position);
+           }
+        public void Update(GameTime gameTime)
+           {
+               slotAnimation.Update(gameTime);
+           }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            slotAnimation.Draw(spriteBatch);
+        }
+    }
+}
